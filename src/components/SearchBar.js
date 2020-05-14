febcc4b28af3194e46994b3fd9fa47e3f211997b
Luma Arruda <luma.oarruda@gmail.com>
2020-05-13 23:19:01 -0300

    css barra de pesquisa

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 class SearchBar extends React.Component {
   constructor(props) {
@@ -14,8 +16,9 @@ class SearchBar extends React.Component {
 
   render () {
     return (
-      <section>
-         <input type="text" onChange={this.setSearch}/>
+      <section className="input-wrapper">
+        <FontAwesomeIcon icon={faSearch} className="searchIcon" />
+        <input type="text" onChange={this.setSearch}/>
       </section>
     )
   }
