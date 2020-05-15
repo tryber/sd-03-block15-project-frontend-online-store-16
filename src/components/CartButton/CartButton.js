4566d14844c8eb8c72b11ebc22ce2e5d58a633f6
Luma Arruda <luma.oarruda@gmail.com>
2020-05-15 18:24:46 -0300

    correçoes do code climate

--- src/components/CartButton/CartButton.js
+++ src/components/CartButton/CartButton.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCartArrowDown } from '@fortawesome/free-solid-svg-icons';
-import './CartButton.css'
+import './CartButton.css';
 
 class CartButton extends React.Component {
   render() {
