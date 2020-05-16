0e8652c56d8b8351bf2f6708422b5d8c7fc8a8f5
Luma Arruda <luma.oarruda@gmail.com>
2020-05-16 01:26:37 -0300

    correçoes do code climate

--- src/components/BackButton/BackButton.js
+++ src/components/BackButton/BackButton.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBackward } from '@fortawesome/free-solid-svg-icons';
-import './BackButton.css'
+import './BackButton.css';
 
 class BackButton extends React.Component {
   render() {
--- src/pages/CartPage/CartPage.js
+++ src/pages/CartPage/CartPage.js
@@ -3,7 +3,7 @@ import Header from '../../components/Header/Header.js';
 import BackButton from '../../components/BackButton/BackButton.js';
 import EmptyCart from '../../images/emptyCart.png';
 import Wheel from '../../images/wheel.png';
-import './CartPage.css'
+import './CartPage.css';
 
 class CartPage extends React.Component {
   render() {
@@ -18,10 +18,10 @@ class CartPage extends React.Component {
           <p className="sale-text">Não deixe de aproveitar nossas promoções!</p>
           <img src={EmptyCart} alt="Empty Cart Page Icon" className="empty-cart-image" />
           <div className="wheel-container">
-            <img src={Wheel} alt="Wheel rolling animation" className="wheel1" /> 
+            <img src={Wheel} alt="Wheel rolling animation" className="wheel1" />
             <img src={Wheel} alt="Wheel rolling animation" className="wheel2" />
           </div>
-       </section>
+        </section>
       </main>
     );
   }
