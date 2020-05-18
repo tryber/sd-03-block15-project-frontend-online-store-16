c3ba7e3a22a3c8953b3c54e93ae77dd2dd491043
tamagoshii <estelaap_martins@hotmail.com>
2020-05-18 17:41:32 -0300

    CC

--- src/components/EmptyCart/EmptyCart.jsx
+++ src/components/EmptyCart/EmptyCart.jsx
@@ -6,15 +6,15 @@ class EmptyCart extends React.Component {
   render() {
     return (
       <section className="empty-container">
-          <h3 data-testid="shopping-cart-empty-message" className="empty-message">
-            Seu carrinho está vazio
-          </h3>
-          <p className="sale-text">Não deixe de aproveitar nossas promoções!</p>
-          <img src={EmptyCartImage} alt="Empty Cart Page Icon" className="empty-cart-image" />
-          <div className="wheel-container">
-            <img src={Wheel} alt="Wheel rolling animation" className="wheel1" />
-            <img src={Wheel} alt="Wheel rolling animation" className="wheel2" />
-          </div>
+        <h3 data-testid="shopping-cart-empty-message" className="empty-message">
+          Seu carrinho está vazio
+        </h3>
+        <p className="sale-text">Não deixe de aproveitar nossas promoções!</p>
+        <img src={EmptyCartImage} alt="Empty Cart Page Icon" className="empty-cart-image" />
+        <div className="wheel-container">
+          <img src={Wheel} alt="Wheel rolling animation" className="wheel1" />
+          <img src={Wheel} alt="Wheel rolling animation" className="wheel2" />
+        </div>
       </section>
     );
   }
